0bb5cfaebd9571f6b24d687bbe1074582864ba09
rmarinskyi <newromka@gmail.com>
2024-12-08 18:51:33 +0200

    Initial commit

---
+++ src/test/java/io/testomat/e2e_tests_light_1/ProjectPageTests.java
@@ -0,0 +1,34 @@
+package io.testomat.e2e_tests_light_1;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ProjectPageTests {
+
+    @Test
+    public void userCanFindProjectWithTests() {
+        open("https://app.testomat.io/");
+
+        //login user
+        $("#content-desktop #user_email").setValue("newromka@gmail.com");
+        $("#content-desktop #user_password").setValue("c27sXAn#8R5hQUd");
+        $("#content-desktop #user_remember_me").click();
+        $("#content-desktop [name=\"commit\"]").click();
+        $(".common-flash-success").shouldBe(visible);
+
+        //search project
+        $("#search").setValue("manufacture light");
+
+        //select project
+        $(byText("Manufacture light")).click();
+
+        //wait for project is loaded
+        $("h2").shouldHave(text("Manufacture light"));
+    }
+
+}
