0db7cd5fb029cc124a164a6576890b77cac32e75
rmarinskyi <newromka@gmail.com>
2024-12-15 18:47:05 +0200

    second lesson

---
+++ src/main/java/io/testomat/e2e_tests_light_1/utils/StringParsers.java
@@ -0,0 +1,17 @@
+package io.testomat.e2e_tests_light_1.utils;
+
+import org.jetbrains.annotations.NotNull;
+
+public class StringParsers {
+
+    public static Integer parseIntegerFromString(String targetText) {
+        String digitText = targetText.replaceAll("\\D+", "");
+        return Integer.parseInt(digitText);
+    }
+
+    public static Double parseDoubleFromString(String targetText) {
+        String doubleText = targetText.replaceAll("[^\\d.]", ""); // Keeps only digits and the decimal point
+        return Double.parseDouble(doubleText);
+    }
+
+}
--- src/test/java/io/testomat/e2e_tests_light_1/ProjectPageTests.java
+++ src/test/java/io/testomat/e2e_tests_light_1/ProjectPageTests.java
@@ -1,34 +1,127 @@
 package io.testomat.e2e_tests_light_1;
 
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.testomat.e2e_tests_light_1.utils.StringParsers;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static io.testomat.e2e_tests_light_1.utils.StringParsers.parseIntegerFromString;
 
-public class ProjectPageTests {
+public class ProjectPageTests extends BaseTest {
+
+    static String baseUrl = env.get("BASE_URL");
+    static String username = env.get("USERNAME");
+    static String password = env.get("PASSWORD");
+    String targetProjectName = "Manufacture light";
+
+    @BeforeAll
+    static void openTestomatAndLogin() {
+        open(baseUrl);
+
+        loginUser(username, password);
+    }
+
+    @BeforeEach
+    void openHomePage() {
+        open(baseUrl);
+    }
+
 
     @Test
     public void userCanFindProjectWithTests() {
-        open("https://app.testomat.io/");
+        searchForProject(targetProjectName);
 
-        //login user
-        $("#content-desktop #user_email").setValue("newromka@gmail.com");
-        $("#content-desktop #user_password").setValue("c27sXAn#8R5hQUd");
-        $("#content-desktop #user_remember_me").click();
-        $("#content-desktop [name=\"commit\"]").click();
-        $(".common-flash-success").shouldBe(visible);
+        selectProject(targetProjectName);
 
-        //search project
-        $("#search").setValue("manufacture light");
-
-        //select project
-        $(byText("Manufacture light")).click();
-
-        //wait for project is loaded
-        $("h2").shouldHave(text("Manufacture light"));
+        waitForProjectPageIsLoaded(targetProjectName);
     }
+
+    @Test
+    public void anotherTest() {
+        searchForProject(targetProjectName);
+
+        SelenideElement targetProject = countOfProjectsShouldBeEqualTo(1).first();
+
+        countOfTestsCasesShouldBeEqualTo(targetProject, 0);
+
+        totalCountOfTestCasesGraterThan(100);
+    }
+
+    @Test
+    public void exampleAssertDouble() {
+        var text = "15.4 coverage";
+        Double actualDouble = StringParsers.parseDoubleFromString(text);
+
+        Assertions.assertTrue(15.4 >= actualDouble);
+    }
+
+    @Test
+    public void exampleParseInteger() {
+        var text = "0 tests";
+        Integer actual = parseIntegerFromString(text);
+
+        Assertions.assertEquals(0, actual);
+    }
+
+    @Test
+    public void exampleParseBoolean() {
+        var text = "true";
+        Boolean actual = Boolean.parseBoolean(text);
+
+        Assertions.assertEquals(true, actual);
+    }
+
+
+    private void waitForProjectPageIsLoaded(String targetProjectName) {
+        $(".first h2").shouldHave(text(targetProjectName));
+        $(".first [href*='/readme']").shouldHave(text("Readme"));
+    }
+
+    private void selectProject(String targetProjectName) {
+        $(byText(targetProjectName)).click();
+    }
+
+    private void searchForProject(String targetProjectName) {
+        $("#search").setValue(targetProjectName);
+    }
+
+    private void totalCountOfTestCasesGraterThan(int expectedTotalCount) {
+        String totalProjects = $("#container kbd").getText();
+        Integer actualCountOfTotalTests = parseIntegerFromString(totalProjects);
+        Assertions.assertTrue(actualCountOfTotalTests > expectedTotalCount);
+    }
+
+
+    private void countOfTestsCasesShouldBeEqualTo(SelenideElement targetProject, int expectedCount) {
+        String countOfTests = targetProject.$("p").getText();
+        Integer actualCountOfTests = parseIntegerFromString(countOfTests);
+        Assertions.assertEquals(expectedCount, actualCountOfTests);
+    }
+
+    @NotNull
+    private ElementsCollection countOfProjectsShouldBeEqualTo(int expectedSize) {
+        return $$("#grid ul li").filter(visible).shouldHave(size(expectedSize));
+    }
+
+
+    public static void loginUser(String email, String password) {
+        $("#content-desktop #user_email").setValue(email);
+        $("#content-desktop #user_password").setValue(password);
+        $("#content-desktop #user_remember_me").click();
+        $("#content-desktop [name=commit]").click();
+        $(".common-flash-success").shouldBe(visible);
+    }
+
+
+
 
 }
